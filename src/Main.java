06f9e8e9fed4ea27855a10950578958b565be073
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-05-02 14:23:05 +0200

    Add all JRG units

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import Cars.Car;
 import Coordinates.Coordinates;
 import JRG.JRG;
 import JRG.JRGCollection;
@@ -7,9 +6,9 @@ import SKKM.SKKM;
 
 public class Main {
     public static void main(String[] args) {
-        JRGCollection jrgCollection = new JRGCollection(1);
-        JRG JRGSkawina = new JRG("JRG Skawina", new Coordinates(49.97224978359688, 19.79655221139836));
-        jrgCollection.addItem(JRGSkawina);
+        JRGCollection jrgCollection = new JRGCollection(10);
+
+        addJRGs(jrgCollection);
 
         SKKM skkm = new SKKM(jrgCollection);
         Notification fireNotification = new FireNotification(new Coordinates(50.0, 20.0));
@@ -19,5 +18,31 @@ public class Main {
         skkm.receiveNotification(fireNotification2);
         Notification localNotification = new LocalDangerNotification(new Coordinates(50.0, 20.0));
         skkm.receiveNotification(localNotification);
+    }
+
+    private static void addJRGs(JRGCollection jrgCollection){
+        JRG JRGSkawina = new JRG("JRG Skawina", new Coordinates(49.97224978359688, 19.79655221139836));
+        JRG JRGSchool = new JRG("JRG Szkoły aspirantów", new Coordinates(50.077513569634725, 20.03378189976448));
+        JRG LSPAirport = new JRG("LSP Lotnisko w Balicach", new Coordinates(50.07886617770602, 19.79443359213229));
+
+        JRG JRG1 = new JRG("JRG-1", new Coordinates(50.06004384469177, 19.943838869073346));
+        JRG JRG2 = new JRG("JRG-2", new Coordinates(50.033593544233234, 19.936466684417994));
+        JRG JRG3 = new JRG("JRG-3", new Coordinates(50.07576800245592, 19.8880441942018));
+        JRG JRG4 = new JRG("JRG-4", new Coordinates(50.03772559933986, 20.006154067218628));
+        JRG JRG5 = new JRG("JRG-5", new Coordinates(50.092381607916785, 19.92247390769325));
+        JRG JRG6 = new JRG("JRG-6", new Coordinates(50.01620570693408, 20.016006551872703));
+        JRG JRG7 = new JRG("JRG-7", new Coordinates(50.09424633345656, 19.97743701140173));
+
+
+        jrgCollection.addItem(JRGSkawina);
+        jrgCollection.addItem(JRGSchool);
+        jrgCollection.addItem(LSPAirport);
+        jrgCollection.addItem(JRG1);
+        jrgCollection.addItem(JRG2);
+        jrgCollection.addItem(JRG3);
+        jrgCollection.addItem(JRG4);
+        jrgCollection.addItem(JRG5);
+        jrgCollection.addItem(JRG6);
+        jrgCollection.addItem(JRG7);
     }
 }
